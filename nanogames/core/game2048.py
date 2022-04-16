339027328ff6938d23aa8bd4eef7607d2ef470ce
krome0-win <sealminc@gmail.com>
2022-04-17 06:38:31 +0900

    # Various changes
    ## game2048
    Simplify push_num in core.game2048.Game2048
    Simplify sum_num in core.game2048.Game2048
    Simplify move in core.game2048.Game2048
    Add check_nozero in core.game2048.Game2048
    Add check_gameover in core.game2048.Game2048
    
    Test completed

--- nanogames/core/game2048.py
+++ nanogames/core/game2048.py
@@ -33,41 +33,62 @@ class Game2048:
     def move(self, direction: str) -> None:
         direction_num = {'left': 0, 'up': 1, 'right': 2, 'down': 3}
 
-        gamestatus_rot = np.rot90(self.gamestatus, direction_num[direction])
-        gamestatus_rot = self.push_num(gamestatus_rot)
-        gamestatus_rot = self.sum_num(gamestatus_rot)
-        gamestatus_rot = self.push_num(gamestatus_rot)
-        self.gamestatus = np.rot90(gamestatus_rot, -direction_num[direction])
+        self.gamestatus = np.rot90(self.gamestatus, direction_num[direction])
+        self.push_num()
+        self.sum_num()
+        self.push_num()
+        self.gamestatus = np.rot90(self.gamestatus, -direction_num[direction])
 
         self.make_num(1)
 
-    def push_num(self, gamestatus: np.ndarray) -> np.ndarray:
-        size_y, size_x = gamestatus.shape
+        if self.check_nozero:
+            a = self.check_gameover()
+            return a
+        else: return False
+
+    def push_num(self) -> None:
+        size_y, size_x = self.gamestatus.shape
         gamestatus_pushed = np.zeros((size_y, size_x), dtype=np.int8)
 
         for index_y in range(size_y):
             index = 0
 
             for index_x in range(size_x):
-                if gamestatus[index_y, index_x] != 0:
-                    nonzero_num = gamestatus[index_y, index_x]
+                if self.gamestatus[index_y, index_x] != 0:
+                    nonzero_num = self.gamestatus[index_y, index_x]
                     gamestatus_pushed[index_y, index] = nonzero_num
                     index += 1
+        
+        self.gamestatus = gamestatus_pushed
 
-        return gamestatus_pushed
-
-    def sum_num(self, gamestatus: np.ndarray) -> np.ndarray:
-        size_y, size_x = gamestatus.shape
+    def sum_num(self) -> None:
+        size_y, size_x = self.gamestatus.shape
         for index_y in range(size_y):
             for index_x in range(size_x - 1):
-                num = gamestatus[index_y, index_x]
-                num_next = gamestatus[index_y, index_x + 1]
+                num = self.gamestatus[index_y, index_x]
+                num_next = self.gamestatus[index_y, index_x + 1]
 
                 if num != 0 and num == num_next:
-                    gamestatus[index_y, index_x] *= 2
-                    gamestatus[index_y, index_x + 1] = 0
+                    self.gamestatus[index_y, index_x] *= 2
+                    self.gamestatus[index_y, index_x + 1] = 0
 
-        return gamestatus
+    def check_nozero(self) -> bool:
+        zeros_y, zeros_x = np.where(self.gamestatus == 0)
+
+        return len(zeros_y) == 0 and len(zeros_x) == 0
+
+    def check_gameover(self) -> bool:
+        size_y, size_x = self.gamestatus.shape
+        for index_y in range(size_y-1):
+            for index_x in range(size_x-1):
+                num = self.gamestatus[index_y, index_x]
+                num_down = self.gamestatus[index_y + 1, index_x]
+                num_right = self.gamestatus[index_y, index_x + 1]
+
+                if num == num_down: return False
+                if num == num_right: return False
+
+        return True
 
     def show_status(self) -> np.ndarray:
         return self.gamestatus
