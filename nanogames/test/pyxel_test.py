0a1b661c650dc0250073843cf9d8ac86865533a9
krome0-win <sealminc@gmail.com>
2022-04-17 06:40:28 +0900

    Add pyxel folder Add main.py in pyxel

---
+++ nanogames/test/pyxel_test.py
@@ -0,0 +1,5 @@
+import pyxel
+
+class pyxel_app():
+    def __init__(self) -> None:
+        pyxel.init(120, 160, title='2048 Game')
--- nanogames/test/test.py
+++ nanogames/test/test.py
@@ -1,15 +1,20 @@
 from nanogames.core.game2048 import Game2048
 from nanogames.windows.keyinput import search_input
 import time
+import numpy as np
+import sys
 
 a = Game2048(4, 4, 2048)
 
 a.start_game()
+a.gamestatus = np.array([[0,4,8,16],[16,8,4,2],[2,4,8,16],[16,8,4,2]])
 print(a.show_status())
 
 while True:
     key = search_input()
     if key is not None:
-        a.move(key)
+        if a.move(key):
+            print('gameover')
+            sys.exit()
         print(a.show_status())
         time.sleep(0.5)
