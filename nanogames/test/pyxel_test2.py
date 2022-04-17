c42123f9322cd486c940712b0816fbb40b2a014f
krome0 <sealminc@gmail.com>
2022-04-18 02:37:29 +0900

    Add select.py in pyxel Ass game2048.py in pyxel

--- nanogames/test/pyxel_test2.py
+++ nanogames/test/pyxel_test2.py
@@ -1,20 +1,21 @@
+import py
 import pyxel
 
 class pyxel_app():
     def __init__(self) -> None:
         pyxel.init(120, 160, title='2048 Game')
         pyxel.mouse(False)
+        pyxel.run(self.update, self.draw)
         
     def draw(self):
         self.clear_screen()
-
-    def updata(self):
-        if pyxel.btnp(pyxel.KEY_ESCAPE):
-            pyxel.quit()
-
-    def draw_gamebg(self):
-        pyxel.rect()
-
+        pyxel.text(50, 50, '2048', 0)
 
     def clear_screen(self, color:pyxel.colors = 15):
         pyxel.cls(color)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_ESCAPE):
+            pyxel.quit()
+
+pyxel_app()
